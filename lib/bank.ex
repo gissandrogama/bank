c95a91f2193674a40d623185b5951bd8f7dac2d1
gissandrogama <gissandrogama@gmail.com>
2021-03-26 00:49:01 -0300

    fix(doc/bank): add macros of documentation

--- lib/bank.ex
+++ lib/bank.ex
@@ -1,4 +1,21 @@
 defmodule Bank do
+  @moduledoc """
+  Mundulo que contem funções parser para funções de outros modulos.
+  """
+
+  @doc """
+  Função que cria os arquivos para adcionar os dados.
+
+  ## Parametro da função
+
+  ## Informações adicionais
+
+  ## Exemplo
+
+      iex> Bank.start()
+      :ok
+
+  """
   def start do
     File.write!("contas.txt", :erlang.term_to_binary([]))
     File.write!("transactions.txt", :erlang.term_to_binary([]))

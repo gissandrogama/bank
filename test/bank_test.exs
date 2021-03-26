c7af03a720eb65ece83cf31786b7739013a0c9fe
gissandrogama <gissandrogama@gmail.com>
2021-03-26 00:51:17 -0300

    fix(test/bank): correction

--- test/bank_test.exs
+++ test/bank_test.exs
@@ -2,7 +2,7 @@ defmodule BankTest do
   use ExUnit.Case
   doctest Bank
 
-  test "greets the world" do
-    assert Bank.hello() == :world
+  test "start/0" do
+    assert Bank.start() == :ok
   end
 end
